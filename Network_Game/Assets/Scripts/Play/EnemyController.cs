98f14909f340d9afe325c889240051d883ac42d4
HattoriNaoki <toraidennto1323@gmail.com>
2017-06-15 11:58:55 +0900

    コミット

---
+++ Network_Game/Assets/Scripts/Play/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    [Range(-10,-1000)]
+    private float movepower=-100;
+
+	// Use this for initialization
+	void Start () {
+        Move();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void Move()
+    {
+        transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(movepower, 0));
+    }
+}
